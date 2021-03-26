615cc1bb8f6e479ed03fe4f2ae0c4c97960a72e0
Tino Rohmund <tinorohmund@hotmail.de>
2021-03-26 14:49:07 +0100

    passport, user auth and generation WIP

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import * as express from "express";
+import * as passport from "passport";
+// import * as session from "express-session"
+import * as passportLocal from "passport-local"
 import { connect } from "./database/database";
 import { createDummyData } from "./skripts/createDummyData";
 
@@ -18,6 +21,39 @@ app.get("/", (req: any, res: any) => {
     res.send("Hello world!");
 });
 
+
+// create dummy data. TODO: POST
 app.get("/dummyData/create", (req: any, res: any) => {
     createDummyData();
 });
+
+
+// passport stuff
+const session = require('express-session');
+const LocalStrategy = passportLocal.Strategy;
+app.use(session({ secret: 'secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        if (username === "admin" && password === "admin") {
+            return done(null, username);
+        } else {
+            return done("unauthorized access", false);
+        }
+    }
+));
+
+app.get("/login", (req: any, res: any) => {
+    res.send("login Page")
+});
+
+app.get("/signup", (req: any, res: any) => {
+    res.send("signup page")
+});
+
+app.get("/logout", (req: any, res: any) => {
+    req.logout();
+    res.redirect("/login");
+})
